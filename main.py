02fcf323eb8044d5cc39829a6f9d16209316787d
R <saegot@naver.com>
2021-10-05 23:13:27 +0900

    포춘 쿠키 점괘

--- main.py
+++ main.py
@@ -6,13 +6,22 @@ def SelectFortune(fortune_list):
     :return:        점괘결과
     '''
 
-    fortune_list_count = len(fortune_list)
-    random_number = random.randrange(0, fortune_list_count)
-    fortune_result = fortune_list[random_number]
-    print("전체 항목 개수 = " + str(fortune_list_count) + ", 랜덤 숫자 = " + str(random_number))
+    fortune_cookie = []
+
+    #점괘 쿠키에 집어넣기
+    for fortune in fortune_list:
+        cookie_count = fortune[1]
+        for n in range(cookie_count):
+            fortune_cookie.append(fortune[0])
+
+    number_of_cookies = len(fortune_cookie)
+    random_number = random.randrange(0, number_of_cookies)
+    fortune_result = fortune_cookie[random_number]
+    print("쿠키 내용 =", fortune_cookie)
+    print("전체 쿠키 개수 = " + str(number_of_cookies) + ", 랜덤 숫자 = " + str(random_number))
     return fortune_result
 
 
 if __name__ == '__main__':
-    fortune_list = ["좋음", "보통", "나쁨"]
+    fortune_list = [["좋음", 3], ["보통", 2], ["나쁨", 1]]
     print(SelectFortune(fortune_list))
