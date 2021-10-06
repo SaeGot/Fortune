668fc8aa3391f0eda7ebad51aa1859cca88634dc
R <saegot@naver.com>
2021-10-06 21:35:24 +0900

    포춘쿠키 깨부수어서 점괘 뽑기

--- main.py
+++ main.py
@@ -1,27 +1,47 @@
 import random
 
-def SelectFortune(fortune_list):
+
+def SelectFortune(fortune_cookies):
     '''
+    점괘 뽑기
+    
+    :param fortune_cookies: 포춘쿠키 리스트
+    :return:                점괘결과
+    '''
+
+    print("포춘쿠키 점괘 뽑기 시작")
+    number_of_cookies = len(fortune_cookies)
+    random_number = random.randrange(0, number_of_cookies)
+    print("쿠키 내용 =", fortune_cookies)
+    print("전체 쿠키 개수 = " + str(number_of_cookies) + ", 랜덤 숫자 = " + str(random_number))
+    fortune_result = fortune_cookies.pop(random_number)
+    print("뽑힌 점괘 = " + fortune_result + "\n")
+    return fortune_result
+
+
+def MakeFortuneCookies(fortune_list):
+    '''
+    점괘 포춘쿠키에 넣기
+    
     :param fortune_list:    점괘결과 목록
-    :return:        점괘결과
+    :return:                포춘쿠키 리스트
     '''
 
-    fortune_cookie = []
-
+    fortune_cookies = []
     #점괘 쿠키에 집어넣기
     for fortune in fortune_list:
         cookie_count = fortune[1]
         for n in range(cookie_count):
-            fortune_cookie.append(fortune[0])
+            fortune_cookies.append(fortune[0])
 
-    number_of_cookies = len(fortune_cookie)
-    random_number = random.randrange(0, number_of_cookies)
-    fortune_result = fortune_cookie[random_number]
-    print("쿠키 내용 =", fortune_cookie)
-    print("전체 쿠키 개수 = " + str(number_of_cookies) + ", 랜덤 숫자 = " + str(random_number))
-    return fortune_result
+    print("포춘 쿠키 완성, 쿠키내용 =", fortune_cookies, "\n")
+    return fortune_cookies
 
 
 if __name__ == '__main__':
     fortune_list = [["좋음", 3], ["보통", 2], ["나쁨", 1]]
-    print(SelectFortune(fortune_list))
+
+    fortune_cookies = MakeFortuneCookies(fortune_list)
+    SelectFortune(fortune_cookies)
+    SelectFortune(fortune_cookies)
+    SelectFortune(fortune_cookies)
