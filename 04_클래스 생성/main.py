d3344657485653762e40373b8c070480ff5c6f45
R <saegot@naver.com>
2021-10-24 14:50:19 +0900

    04

---
+++ 04_클래스 생성/main.py
@@ -0,0 +1,10 @@
+import FortuneCookie
+
+
+if __name__ == '__main__':
+    fortune_list = [["좋음", 3], ["보통", 2], ["나쁨", 1]]
+
+    fortune_cookie = FortuneCookie.FortuneCookie(fortune_list)
+    fortune_cookie.SelectFortune()
+    fortune_cookie.SelectFortune()
+    fortune_cookie.SelectFortune()
